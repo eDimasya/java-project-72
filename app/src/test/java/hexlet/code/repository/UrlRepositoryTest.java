e8d25eb01d18752cc9e4fe5900d22ff8be1d43c2
Dima <edimasya@gmail.com>
2024-12-25 15:54:52 +0100

    update checkstyle

--- app/src/test/java/hexlet/code/repository/UrlRepositoryTest.java
+++ app/src/test/java/hexlet/code/repository/UrlRepositoryTest.java
@@ -1,8 +1,6 @@
 package hexlet.code.repository;
 
-import hexlet.code.model.Url;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class UrlRepositoryTest {
